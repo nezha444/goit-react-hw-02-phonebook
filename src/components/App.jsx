1ead9eae3c25f8cd1c743ca517d61ce296957f18
Nikita <bazilik2452@gmail.com>
2023-05-31 23:02:26 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './Phonebook/ContactForm';
+import ContactList from './Phonebook/ContactList';
+import Filter from './Phonebook/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  hendleChangeFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget
+    const name = form.elements.name.value
+    const number = form.elements.number.value
+    const newContact = {
+      id: `id-${this.state.contacts.length + 1}`,
+      name: name,
+      number: number,
+    };
+    
+    if (this.state.contacts.some(el => el.name === name && el.number === number)) {
+      alert('Contact already exists');
+    } else {
+      this.setState(prevState => ({
+    contacts: [...prevState.contacts, newContact],
+  }));
+    }
+
+    form.reset();
+  };
+
+  deleteContact = (event) => {
+    console.log('sadsadas');
+
+  }
+  
+
+  render() {
+    const { contacts, name } = this.state;
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <ContactForm
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
+        <Filter hendleChangeFilter={this.hendleChangeFilter} />
+        <ContactList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Phonebook/ContactForm.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class ContactForm extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit}>
+          <label>
+            Name
+            <input
+              // onChange={this.props.handleChange}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            Number
+            <input
+              // onChange={this.props.handleChange}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
+    );
+  }
+}
